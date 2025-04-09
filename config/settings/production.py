e556804d6c5a1d1debb8bc1a9022dfebd0df3566
Bill HOUNMENOU <b.hounmenou@kohe.fr>
2025-04-09 16:42:25 +0200

    version ok_push_online

---
+++ config/settings/production.py
@@ -0,0 +1,28 @@
+from .base import *
+from .base import config
+
+import dj_database_url
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config("DB_NAME"),
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': config("DB_HOST", "localhost"),
+#         'PORT': config("DB_PORT"),
+#     }
+# }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        engine='django.contrib.gis.db.backends.postgis'
+    )
+}
